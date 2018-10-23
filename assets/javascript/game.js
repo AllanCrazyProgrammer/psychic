df19c8f32d8b1b63da0172491f1f54d97f9c24db
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2018-10-23 18:16:18 -0500

    wins & guesses so far DONE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,40 @@
 
 var randLetter;
-var myLetter;
+var myGuess;
+var guessesSoFar = [];
+var winsCounter = 0;
 
-function randomLetter(){
+
+
+function randomLetter() {
+
     var possibleLetter = "qwertyuiopasdfghjklñzxcvbnm";
-    randLetter = possibleLetter.charAt(Math.floor(Math.random()*possibleLetter.length));
+    randLetter = possibleLetter.charAt(Math.floor(Math.random() * possibleLetter.length));
     return randLetter;
 }
 
-document.onkeyup = function(){
-    myLetter = event.key;
-    return myLetter;
+document.onkeyup = function () {
+    myGuess = (event.key);
+    guessesSoFar.push(event.key);
+    document.getElementById("guesses").innerHTML = ("Guesses so far: " + guessesSoFar);
+    if (myGuess === randLetter){
+        winsCounter++;
+        document.getElementById("wins").innerHTML = ("Wins: " + winsCounter);
+        randomLetter();
+        console.log(randLetter);
+    }
 }
 
 
+
+
+
+
+
+
 randomLetter();
+
+
 
 console.log(randLetter);
 
