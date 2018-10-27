e784329134155a68bb40cb2f38f60ed0c5f66694
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2018-10-27 13:58:33 -0500

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,37 @@
-
-var randLetter;
 var myGuess;
 var guessesSoFar = [];
 var winsCounter = 0;
-
-
-
-function randomLetter() {
-
-    var possibleLetter = "qwertyuiopasdfghjklñzxcvbnm";
-    randLetter = possibleLetter.charAt(Math.floor(Math.random() * possibleLetter.length));
-    return randLetter;
-}
+var lossCounter = 0;
+var livesCounter = 10;
+var possibleLetter = "qwertyuiopasdfghjklñzxcvbnm";
+var randLetter = possibleLetter.charAt(Math.floor(Math.random() * possibleLetter.length));
 
 document.onkeyup = function () {
     myGuess = (event.key);
     guessesSoFar.push(event.key);
-    document.getElementById("guesses").innerHTML = ("Guesses so far: " + guessesSoFar);
-    if (myGuess === randLetter){
+
+    if (myGuess === randLetter) {
+        randLetter = possibleLetter.charAt(Math.floor(Math.random() * possibleLetter.length));
         winsCounter++;
-        document.getElementById("wins").innerHTML = ("Wins: " + winsCounter);
-        randomLetter();
+        livesCounter = 10;
+        guessesSoFar = [];
         console.log(randLetter);
+
+    } else {
+        livesCounter--;
+
     }
+    if (livesCounter === 0) {
+        alert("game over");
+        lossCounter++;
+        livesCounter = 10;
+        winsCounter = 0;
+        guessesSoFar = [];
+
+    }
+
+    document.getElementById("guesses").innerHTML = ("Guesses so far: " + guessesSoFar);
+    document.getElementById("wins").innerHTML = ("Wins: " + winsCounter);
+    document.getElementById("lives").innerHTML = ("Guesses left: " + livesCounter);
+    document.getElementById("loses").innerHTML = ("Loses: " + lossCounter);
 }
-
-
-
-
-
-
-
-
-randomLetter();
-
-
-
-console.log(randLetter);
-
